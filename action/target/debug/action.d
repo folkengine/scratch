d0bc5492ebee9c1090cf271e4bc2ffb7cfb45f25
ElectronicPanopticon <gaoler@electronicpanopticon.com>
2022-03-05 09:49:52 -0800

    gitignore

--- action/target/debug/action.d
+++
@@ -1,1 +0,0 @@
-/home/gaoler/src/github.com/folkengine/scratch/action/target/debug/action: /home/gaoler/src/github.com/folkengine/scratch/action/src/main.rs
--- action/target/debug/deps/action-3429eed9ffce5fa2.d
+++
@@ -1,5 +0,0 @@
-/home/gaoler/src/github.com/folkengine/scratch/action/target/debug/deps/action-3429eed9ffce5fa2: src/main.rs
-
-/home/gaoler/src/github.com/folkengine/scratch/action/target/debug/deps/action-3429eed9ffce5fa2.d: src/main.rs
-
-src/main.rs:
