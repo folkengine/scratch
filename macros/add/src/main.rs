247e0ea4b47ebee9e0e4d85c319968c646e47cc4
ElectronicPanopticon <gaoler@electronicpanopticon.com>
2022-03-25 12:47:50 -0700

    Added TT Mumcher to add

--- macros/add/src/main.rs
+++ macros/add/src/main.rs
@@ -1,12 +1,3 @@
-macro_rules! add {
-    ($a: expr, $b: expr) => {{
-        $a + $b
-    }};
-    ($a: expr) => {{
-        $a
-    }};
-}
-
 macro_rules! add_as {
     ( $($a:expr), * ) => {
         {
@@ -16,9 +7,24 @@ macro_rules! add_as {
     }
 }
 
+macro_rules! add {
+    ($a: expr) => {
+        $a
+    };
+    // ($a: expr, $b: expr) => {
+    //     $a + $b
+    // };
+    // TT Muncher
+    ($a: expr, $($b:tt)*) => {
+        $a+add!($($b)*)
+    }
+}
+
 fn main() {
     let r = add!(1, 2);
     println!("r = {}", r);
     println!("r = {}", add!(45));
     println!("r = {}", add_as!(45, 44, 43));
+    println!("{}", add_as!(1, 2, 3, 4));
+    println!("{}", add!(1, 2, 3, 4));
 }
